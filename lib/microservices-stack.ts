cb925726a10b34e485a4933f63065665dce342cf
Freilin Manzano <freilin.manzano@externos.bci.cl>
2022-08-23 18:31:57 -0400

    create basket lambda

--- lib/microservices-stack.ts
+++ lib/microservices-stack.ts
@@ -1,16 +1,23 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { SwnApiGateway } from './apigateway';
+import { SwnDatabase } from './database';
+import { SwnMicroservices } from './microservice';
 
-export class MicroservicesStack extends Stack {
+export class AwsMicroservicesStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const database = new SwnDatabase(this, 'Database');    
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MicroservicesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const microservices = new SwnMicroservices(this, 'Microservices', {
+      productTable: database.productTable,
+      basketTable: database.basketTable
+    });
+
+    const apigateway = new SwnApiGateway(this, 'ApiGateway', {
+      productMicroservice: microservices.productMicroservice,
+      basketMicroservice: microservices.basketMicroservice
+    });    
   }
 }
